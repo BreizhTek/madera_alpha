c8abfae094b7485a9f39a3f6af8803261413f99f
unknown <timothe.laine@gmail.com>
2021-02-04 23:14:55 +0100

    Client list ui build

--- lib/src/views/ui/client_list.dart
+++ lib/src/views/ui/client_list.dart
@@ -1,6 +1,8 @@
 import 'package:dart_strapi/dart_strapi.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:madera_prototype/src/buisness_logic/services/api_services/api.dart';
+import 'package:madera_prototype/src/buisness_logic/utils/configuration.dart';
 import 'package:madera_prototype/src/views/components/reusable_widgets.dart';
 import 'package:madera_prototype/src/views/utils/style.dart';
 
@@ -14,7 +16,7 @@ class ClientsList extends StatefulWidget {
 class _ClientsList extends State<ClientsList> {
   List<ContentType> _clientsList;
 
-  Future<bool> getClientsList () async {
+  Future<bool> getClientsList() async {
     this._clientsList = await Api.getClients();
     return true;
   }
@@ -25,33 +27,27 @@ class _ClientsList extends State<ClientsList> {
       // appBar: reusableWidgets.header(''),
       bottomNavigationBar: reusableWidgets.bottomBar(),
       body: SafeArea(
-          child: FutureBuilder<bool> (
+          child: FutureBuilder<bool>(
               future: this.getClientsList(),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  return Container(
+                  return SingleChildScrollView(
                       padding: const EdgeInsets.symmetric(horizontal: 20),
                       child: Column(
                         children: rowClients(),
                       )
-                    // child: SearchBar(),
-                  );
+                      // child: SearchBar(),
+                      );
                 } else {
-                  return Container(
-                  );
+                  return Container();
                 }
-              }
-          )
-      ),
+              })),
     );
   }
 
   Container button(String text, context) {
     return Container(
-      width: MediaQuery
-          .of(context)
-          .size
-          .width / 3.3,
+      width: MediaQuery.of(context).size.width / 3.3,
       margin: new EdgeInsets.symmetric(vertical: 15.0),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(primary: mainBlue),
@@ -64,37 +60,81 @@ class _ClientsList extends State<ClientsList> {
     );
   }
 
-
-  List<Widget> rowClients(){
+  List<Widget> rowClients() {
     List<Widget> rows = [];
 
-    // rows.add(
-    //     Row(
-    //         children: <Widget>[
-    //           Text(''),
-    //           Divider(color: Colors.blue[800]),
-    //         ]
-    //     )
-    // );
-    for(var i=0; i<_clientsList.length; i++) {
-      rows.add(
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.search),
-                  color: Colors.blue[800],
-                  onPressed: () {},//quote.Id
-                ),
-                Text(_clientsList[i].data['Firstname'].toString()),
-                Text(_clientsList[i].data['Lastname'].toString()),
-                Text(_clientsList[i].data['Adresse'].toString()),
+    for (var i = 0; i < _clientsList.length; i++) {
+      rows.add(Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50)
+            ),
+            boxShadow: [
+        BoxShadow(
+        color: Colors.grey.withOpacity(0.1),
+        spreadRadius: 5,
+        blurRadius: 7,
+        offset: Offset(0, 3), // changes position of shadow
+      )]),
+          margin: EdgeInsets.only(top: 10.0),
+          width: isLargeScreen
+              ? MediaQuery.of(context).size.width / 1
+              : MediaQuery.of(context).size.width / 1,
+          child: OutlineButton(
+            onPressed: () {},
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    child:  IconButton(
+                      icon: Icon(Icons.add_outlined),
+                      color: mainBlue,
+                      onPressed: () {}, //quote.Id
+                    ),
+                  ),
+                  Container(
+                    width: isLargeScreen
+                        ? MediaQuery.of(context).size.width / 5
+                        : MediaQuery.of(context).size.width / 3,
+                    child: simpleText(
+                        _clientsList[i].data['Firstname'].toString(), 15),
+                  ),
+                  Container(
+                    width: isLargeScreen
+                        ? MediaQuery.of(context).size.width / 5
+                        : MediaQuery.of(context).size.width / 3,
+                    child: simpleText(
+                        _clientsList[i].data['Lastname'].toString(), 15),
+                  ),
+                  if(isLargeScreen) container(i),
 
-              ]
-          )
-      );
+                ]),
+          )));
     }
     return rows;
   }
 
+  Container container(int index){
+    return Container(
+      width: isLargeScreen
+          ? MediaQuery.of(context).size.width / 3
+          : MediaQuery.of(context).size.width / 3,
+      child: simpleText(
+          _clientsList[index].data['Addresse'].toString(), 12),
+    );
+  }
+  Text simpleText(String text, double size) {
+    return Text(text,
+        textAlign: TextAlign.center,
+        style: GoogleFonts.montserrat(
+          fontSize: isLargeScreen ? size : size - 3,
+          color: mainBlue,
+          fontWeight: FontWeight.w400,
+        ));
+  }
 }
